e8e36a6ded9af9f2d183d71459764c418ab3ef70
Ilya Levin <bskton@gmail.com>
2018-10-13 11:49:00 +1000

    Using merge operator

--- src/code.ts
+++ src/code.ts
@@ -1,22 +1,16 @@
-import { AsyncSubject, Subscription } from 'rxjs';
+import { Observable, Observer } from 'rxjs';
+import { merge } from 'rxjs';
 
-const subject = new AsyncSubject();
+const firstObservable = Observable.create((observer: Observer<string>) => {
+  observer.next('Hey guys!');
+})
 
-const firstSubscription = subject.subscribe(
-  data => addItem('Subscription 1: ' + data),
-  err => addItem(err),
-  () => addItem('Subscription 1 Completed.')
-)
+const secondObservable = Observable.create((observer: Observer<string>) => {
+  observer.next('How is it going?');
+});
 
-let i = 1;
-const interval = setInterval(() => subject.next(i++), 100);
-
-setTimeout(() => {
-  const secondSubscription = subject.subscribe(
-    data => addItem('Subscription 2: ' + data)
-  )
-  subject.complete();
-}, 500)
+const mergedObserable = merge(firstObservable, secondObservable);
+mergedObserable.subscribe((x: string) => addItem(x));
 
 function addItem(val: any): void {
   const node = document.createElement('li');
