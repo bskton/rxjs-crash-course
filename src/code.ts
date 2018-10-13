911e66933478259faa98cfabb676d20eb88d058c
Ilya Levin <bskton@gmail.com>
2018-10-13 12:00:08 +1000

    Using pluck operator and pipe function

--- src/code.ts
+++ src/code.ts
@@ -1,16 +1,17 @@
 import { Observable, Observer } from 'rxjs';
-import { merge } from 'rxjs';
+import { map, pluck } from 'rxjs/operators';
 
-const firstObservable = Observable.create((observer: Observer<string>) => {
-  observer.next('Hey guys!');
-})
-
-const secondObservable = Observable.create((observer: Observer<string>) => {
-  observer.next('How is it going?');
-});
-
-const mergedObserable = merge(firstObservable, secondObservable);
-mergedObserable.subscribe((x: string) => addItem(x));
+const firstObservable = Observable.create(
+  (observer: Observer<{ msg: string }>) => {
+    observer.next({ msg: 'Hey guys!' });
+    observer.next({ msg: 'How are you?' });
+  }
+)
+  .pipe(
+    pluck('msg'),
+    map((val: string) => val.toUpperCase())
+  )
+  .subscribe((x: string) => addItem(x));
 
 function addItem(val: any): void {
   const node = document.createElement('li');
