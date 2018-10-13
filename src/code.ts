6337fe225446855e0afdecb3a929c1a277869172
Ilya Levin <bskton@gmail.com>
2018-10-13 10:14:43 +1000

    Using subject

--- src/code.ts
+++ src/code.ts
@@ -1,12 +1,25 @@
-import { fromEvent, Observable, Observer, Subscription } from 'rxjs';
+import { Subject } from 'rxjs';
 
-const observable: Observable<any> = fromEvent(document, 'mousemove');
+const subject = new Subject();
 
-setTimeout(() => {
-  const subscription: Subscription = observable.subscribe(
-    (x: any) => addItem(x)
-  );
-}, 2000);
+const firstSubscription = subject.subscribe(
+  data => addItem('Subscription 1: ' + data),
+  err => addItem(err),
+  () => addItem('Subscription 1 Completed.')
+)
+
+subject.next('The first thing has been sent.');
+
+const secondSubscription = subject.subscribe(
+  data => addItem('Subscription 2: ' + data)
+)
+
+subject.next('The second thing has been sent.')
+subject.next('The third thing has been sent.');
+
+secondSubscription.unsubscribe();
+
+subject.next('A final thing has been sent.');
 
 function addItem(val: any): void {
   const node = document.createElement('li');
